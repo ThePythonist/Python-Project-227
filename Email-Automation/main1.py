e9d428778e8cf18f80a3d1a714f8dbf3cd3f0609
arash <awrash.sn@gmail.com>
2024-10-10 07:24:20 -0700

    S1

---
+++ Email-Automation/main1.py
@@ -0,0 +1,17 @@
+import smtplib, ssl
+from credentials import password
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "YOUR EMAIL"  # Enter your address
+receiver_email = "RECEIVER EMAIL"  # Enter receiver address
+
+message = """
+Subject: Hi there
+This message is sent from Python.
+"""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
